8dee51d2c7ac1418fda27d745460b584b150d3c1
Albert Lay <albertjlay@gmail.com>
2021-06-28 12:14:00 +0700

    Radical redesign to Board class.

--- src/Board.ts
+++ src/Board.ts
@@ -11,6 +11,8 @@ export default class Board {
    * Array representing the nine squares.
    */
   readonly boardSquares: Square[];
+
+  // ---------------------------- GAME STATES -------------------------------- //
   /**
    * Array representing the IDs of squares occupied by PX.
    * Elements are in original order determined by moves of PX (earliest first).
@@ -27,16 +29,45 @@ export default class Board {
   currentTurn: PlayerID;
 
   /**
+   * Current winning pattern of the game.
+   * If the game has not concluded or result in a draw, will be empty.
+   */
+  curWinPattern: Number[][];
+
+  private _isGameOver = false;
+  /**
+   * Whether the game has concluded or not.
+   */
+  get isGameOver() {
+    return this._isGameOver;
+  }
+  set isGameOver(x: boolean) {
+    if (x && this.curWinPattern.length === 0 && this.xSquares.length + this.oSquares.length !== 9) {
+      throw new Error('Game must be won or board must be full for the game to be over!');
+    }
+    this._isGameOver = x;
+  }
+
+  private _isDraw = false;
+  /**
+   * Whether the game results in a draw or not.
+   */
+  get isDraw() {
+    return this._isDraw;
+  }
+  set isDraw(x: boolean) {
+    if (!x || (this.isGameOver && this.curWinPattern.length === 0)) {
+      this._isDraw = x;
+    } else {
+      throw new Error('Game must conclude and no player must win for the game to draw!');
+    }
+  }
+  // -------------------------------------------------------------------------------------- //
+
+  /**
    * Array containing SquareIDs in ascending order.
    */
   private readonly _squareIDs: SquareID[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  /**
-   * Current result of the game:
-   *  * If a player has won, equals an array of the winning square IDs.
-   *  * If draw, equals [-1]
-   *  * If inconclusive, equals[]
-   */
-  private _curResult: Number[] = [];
 
   /**
    * Creates a new, empty board.
@@ -46,32 +77,16 @@ export default class Board {
     this.xSquares = [];
     this.oSquares = [];
     this.currentTurn = PlayerID.playerX;
+    this.curWinPattern = [];
   }
 
   /**
-   * Determines whether a player has won or a draw has been achieved
-   * @returns ID of winner, 'DRAW', or 'INCONCLUSIVE'
-   */
-  checkResult() {
-    const winningPatternX = this.isWin(this.xSquares);
-    const winningPatternO = this.isWin(this.oSquares);
-    if (winningPatternX) {
-      return winningPatternX;
-    } else if (winningPatternO) {
-      return winningPatternO;
-    } else if (this.xSquares.length + this.oSquares.length === 9) {
-      return [-1];
-    }
-    return [];
-  }
-
-  /**
-   * Determines whether filledSquares has a horizontal winning pattern.
-   * If it does, return an array of the winning square IDs.
+   * Find all horizontal winning patterns of filledSquares.
    * @param - Array of square IDs which have been filled by the player.
-   * @returns Array of winning squares if it has a winning pattern. false otherwise.
+   * @returns Array of array of horizontal-winning squares (in arbitrary order).
    */
-  isHorizontalWin(filledSquares: SquareID[]) {
+  findHorizontalWins(filledSquares: SquareID[]) {
+    const retval: SquareID[][] = [];
     const sortedArr = filledSquares.sort((a, b) => a - b);
     for (let i = 0; i < sortedArr.length - 2; i += 1) {
       const isConsecutive =
@@ -79,88 +94,152 @@ export default class Board {
       const isCorrectColumn =
         sortedArr[i] % 3 === 0 && sortedArr[i + 1] % 3 === 1 && sortedArr[i + 2] % 3 === 2;
       if (isConsecutive && isCorrectColumn) {
-        return sortedArr.slice(i, i + 3);
+        retval.push(sortedArr.slice(i, i + 3));
       }
     }
-    return false;
+    return retval;
   }
 
   /**
-   * Determines whether filledSquares has a vertical winning pattern.
-   * If it does, return an array of the winning square IDs.
+   * Find all vertical winning patterns of filledSquares.
    * @param - Array of square IDs which have been filled by the player.
-   * @returns Array of winning squares if it has a winning pattern. false otherwise.
+   * @returns Array of array of vertical-winning squares (in arbitrary order).
    */
-  isVerticalWin(filledSquares: SquareID[]) {
+  findVerticalWins(filledSquares: SquareID[]) {
+    const retval: SquareID[][] = [];
     for (let i = 0; i < 3; i += 1) {
       const verticalPattern = filledSquares.filter((el) => el % 3 === i);
       if (verticalPattern.length === 3) {
-        return verticalPattern;
+        retval.push(verticalPattern);
       }
     }
-    return false;
+    return retval;
   }
 
   /**
-   * Determines whether filledSquares has a diagonal winning pattern.
-   * If it does, return an array of the winning square IDs.
+   * Find all diagonal winning patterns of filledSquares.
    * @param - Array of square IDs which have been filled by the player.
-   * @returns Array of winning squares if it has a winning pattern. false otherwise.
+   * @returns Array of array of diagonal-winning squares (in arbitrary order).
    */
-  isDiagonalWin(filledSquares: SquareID[]) {
+  findDiagonalWins(filledSquares: SquareID[]) {
+    const retval: SquareID[][] = [];
     const lrDiagonalPattern = filledSquares.filter((el) => el === 0 || el === 4 || el === 8);
     if (lrDiagonalPattern.length === 3) {
-      return lrDiagonalPattern;
+      retval.push(lrDiagonalPattern);
     }
     const rlDiagonalPattern = filledSquares.filter((el) => el === 2 || el === 4 || el === 6);
     if (rlDiagonalPattern.length === 3) {
-      return rlDiagonalPattern;
+      retval.push(rlDiagonalPattern);
     }
-    return false;
-  }
-
-  isWin(filledSquares: SquareID[]): false | SquareID[] {
-    return (
-      this.isHorizontalWin(filledSquares) ||
-      this.isVerticalWin(filledSquares) ||
-      this.isDiagonalWin(filledSquares)
-    );
-  }
-
-  winHandler() {
-    this.boardSquares.forEach((el) => {
-      el.isActive = false;
-      if (this._curResult.includes(el.id)) {
-        el.isWin = true;
-      }
-    });
+    return retval;
   }
 
   /**
-   * Render the board with nine empty squares as children to parent.
-   * @param parent The HTML element where the squares will be appended to.
+   * Find all winning patterns of filledSquares.
+   * @param - Array of square IDs which have been filled by the player.
+   * @returns Array of array of winning squares (in arbitrary order).
    */
-  render(parent: HTMLElement) {
-    const boardDOM = document.createElement('div');
-    boardDOM.classList.add('board');
-    this.boardSquares.forEach((square) => {
-      square.render(boardDOM, () => {
-        square.isActive = false;
-        if (this.currentTurn === PlayerID.playerX) {
-          this.xSquares.push(square.id);
-          square.state = PlayerID.playerX;
-          this.currentTurn = PlayerID.playerO;
-        } else {
-          this.oSquares.push(square.id);
-          square.state = PlayerID.playerO;
-          this.currentTurn = PlayerID.playerX;
-        }
-        this._curResult = this.checkResult();
-        if (this._curResult.length === 3) {
-          this.winHandler();
-        }
-      });
-    });
-    parent.appendChild(boardDOM);
+  findWins(filledSquares: SquareID[]) {
+    return [
+      ...this.findHorizontalWins(filledSquares),
+      ...this.findVerticalWins(filledSquares),
+      ...this.findDiagonalWins(filledSquares),
+    ];
   }
+
+  /**
+   * Should be called when a player moves. Will update all game states.
+   * @param move Which square the player is making their move.
+   * @param player Which player is moving.
+   */
+  playerMove(move: SquareID, player: PlayerID) {
+    if (this.xSquares.includes(move) || this.oSquares.includes(move)) {
+      throw new Error('Player cannot occupy a previously occupied square!');
+    } else if (player === this.currentTurn) {
+      throw new Error('Player can only move when it is their turn!');
+    }
+
+    if (player === PlayerID.playerX) {
+      this.xSquares.push(move);
+    } else {
+      this.oSquares.push(move);
+    }
+    this.updateGameState();
+  }
+
+  /**
+   * Update game states: curWinPattern, isGameOver, currentTurn and isDraw.
+   * [Must only be called by playerMove]
+   */
+  private updateGameState() {
+    // Check whether game has concluded
+    this.curWinPattern = [...this.findWins(this.xSquares), ...this.findWins(this.oSquares)];
+    if (this.curWinPattern.length !== 0) {
+      this.isGameOver = true;
+    } else if (this.xSquares.length + this.oSquares.length === 9) {
+      this.isGameOver = true;
+      this.isDraw = true;
+    }
+
+    // Change next turn player
+    if (this.currentTurn === PlayerID.playerX) {
+      this.currentTurn = PlayerID.playerO;
+    } else {
+      this.currentTurn = PlayerID.playerX;
+    }
+  }
+
+  // /**
+  //  * Determines whether a player has won or a draw has been achieved
+  //  * @returns ID of winner, 'DRAW', or 'INCONCLUSIVE'
+  //  */
+  // checkResult() {
+  //   const winningPatternX = this.findWins(this.xSquares);
+  //   const winningPatternO = this.findWins(this.oSquares);
+  //   if (winningPatternX) {
+  //     return winningPatternX;
+  //   } else if (winningPatternO) {
+  //     return winningPatternO;
+  //   } else if (this.xSquares.length + this.oSquares.length === 9) {
+  //     return [-1];
+  //   }
+  //   return [];
+  // }
+  // winHandler() {
+  //   this.boardSquares.forEach((el) => {
+  //     el.isActive = false;
+  //     if (this.curWinPattern.includes(el.id)) {
+  //       el.isWin = true;
+  //     }
+  //   });
+  // }
+
+  // /**
+  //  * Render the board with nine empty squares as children to parent.
+  //  * @param parent The HTML element where the squares will be appended to.
+  //  */
+  // render(parent: HTMLElement) {
+  //   const boardDOM = document.createElement('div');
+  //   boardDOM.classList.add('board');
+  //   this.boardSquares.forEach((square) => {
+  //     square.render(boardDOM, () => {
+  //       square.isActive = false;
+  //       if (this.currentTurn === PlayerID.playerX) {
+  //         this.xSquares.push(square.id);
+  //         square.state = PlayerID.playerX;
+  //         this.curWinPattern = this.findWins(this.xSquares);
+  //         this.currentTurn = PlayerID.playerO;
+  //       } else {
+  //         this.oSquares.push(square.id);
+  //         square.state = PlayerID.playerO;
+  //         this.curWinPattern = this.findWins(this.oSquares);
+  //         this.currentTurn = PlayerID.playerX;
+  //       }
+  //       if (this.curWinPattern.length !== 0) {
+  //         this.winHandler();
+  //       }
+  //     });
+  //   });
+  //   parent.appendChild(boardDOM);
+  // }
 }
--- tests/Board.test.ts
+++ tests/Board.test.ts
@@ -12,57 +12,97 @@ test('Initialization of board', () => {
   });
 });
 
-test('Win condition', () => {
+test('Check winning patterns', () => {
   const testBoard = new Board();
-  const checkWinningPattern = (board: Board, arr: number[]) => {
-    expect(board.checkResult().length).toBe(arr.length);
-    arr.forEach((el) => {
-      expect(board.checkResult()).toContain(el);
+  const checkWinningPattern = (board: Board, expectedWinPattern: number[][]) => {
+    const computedWinPattern = [
+      ...board.findWins(board.xSquares),
+      ...board.findWins(board.oSquares),
+    ];
+
+    // Sorting both arrays (so order does not matter)
+    computedWinPattern.map((pattern) => pattern.sort((a, b) => a - b));
+    expectedWinPattern.map((pattern) => pattern.sort((a, b) => a - b));
+
+    expect(computedWinPattern.length).toBe(expectedWinPattern.length);
+    expectedWinPattern.forEach((pattern) => {
+      expect(computedWinPattern).toContainEqual(pattern);
     });
   };
 
+  // ------------------------ Single Pattern -------------------- //
   // Horizontal
   testBoard.xSquares = [0, 1, 2];
   testBoard.oSquares = [4, 5];
-  checkWinningPattern(testBoard, testBoard.xSquares);
+  checkWinningPattern(testBoard, [testBoard.xSquares]);
   testBoard.xSquares = [0, 7, 6, 1];
   testBoard.oSquares = [3, 4, 5];
-  checkWinningPattern(testBoard, testBoard.oSquares);
+  checkWinningPattern(testBoard, [testBoard.oSquares]);
   testBoard.xSquares = [7, 6, 8];
   testBoard.oSquares = [3, 4];
-  checkWinningPattern(testBoard, testBoard.xSquares);
+  checkWinningPattern(testBoard, [testBoard.xSquares]);
 
   // Vertical
   testBoard.xSquares = [0, 6, 3];
   testBoard.oSquares = [4, 5];
-  checkWinningPattern(testBoard, testBoard.xSquares);
+  checkWinningPattern(testBoard, [testBoard.xSquares]);
   testBoard.xSquares = [0, 2, 6, 8];
   testBoard.oSquares = [7, 4, 1];
-  checkWinningPattern(testBoard, testBoard.oSquares);
+  checkWinningPattern(testBoard, [testBoard.oSquares]);
   testBoard.xSquares = [2, 5, 8];
   testBoard.oSquares = [3, 4];
-  checkWinningPattern(testBoard, testBoard.xSquares);
+  checkWinningPattern(testBoard, [testBoard.xSquares]);
 
   // Diagonal
   testBoard.xSquares = [8, 0, 4];
   testBoard.oSquares = [1, 2];
-  checkWinningPattern(testBoard, testBoard.xSquares);
+  checkWinningPattern(testBoard, [testBoard.xSquares]);
   testBoard.xSquares = [0, 1, 8, 5];
   testBoard.oSquares = [2, 4, 6];
-  checkWinningPattern(testBoard, testBoard.oSquares);
-
-  // Draw
-  testBoard.xSquares = [0, 2, 3, 5, 7];
-  testBoard.oSquares = [1, 4, 6, 8];
-  checkWinningPattern(testBoard, [-1]);
-
-  // Should not win.
-  testBoard.xSquares = [0, 4, 5, 6];
-  testBoard.oSquares = [1, 3, 7];
-  checkWinningPattern(testBoard, []);
+  checkWinningPattern(testBoard, [testBoard.oSquares]);
 
   // Win when full
   testBoard.xSquares = [0, 4, 5, 6, 8];
   testBoard.oSquares = [1, 2, 3, 7];
-  checkWinningPattern(testBoard, [0, 4, 8]);
+  checkWinningPattern(testBoard, [[0, 4, 8]]);
+
+  // ------------------------ Multiple patterns -------------------- //
+  testBoard.xSquares = [0, 2, 4, 6, 8];
+  testBoard.oSquares = [1, 3, 5, 7];
+  checkWinningPattern(testBoard, [
+    [0, 4, 8],
+    [2, 4, 6],
+  ]);
+
+  testBoard.xSquares = [0, 1, 3, 4];
+  testBoard.oSquares = [2, 5, 6, 7, 8];
+  checkWinningPattern(testBoard, [
+    [2, 5, 8],
+    [6, 7, 8],
+  ]);
+
+  testBoard.xSquares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  testBoard.oSquares = [];
+  checkWinningPattern(testBoard, [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]);
+});
+
+test('Check draw and inconclusive patterns', () => {
+  // // ---------------------------- Draw ------------------------- //
+  // testBoard.xSquares = [0, 2, 3, 5, 7];
+  // testBoard.oSquares = [1, 4, 6, 8];
+  // checkWinningPattern(testBoard, [-1]);
+  // // ---------------------------- None --------------------------- //
+  // testBoard.xSquares = [0, 4, 5, 6];
+  // testBoard.oSquares = [1, 3, 7];
+  // checkWinningPattern(testBoard, []);
+  //  // ------------------------------------------------------------ //
 });
