23d197a2c51403ff05d5ef5ff8b4870f08259285
Albert Lay <albertjlay@gmail.com>
2021-06-29 22:25:32 +0700

    Add game mode buttons

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,50 @@ import Board from './Board';
 import DumbAIOpponent from './DumbAIOpponent';
 import { PlayerID } from './types';
 
-/**
- * Clears root div and instantiate a new board.
- */
-const newGame = function () {
-  console.log('run');
+// /**
+//  * Clears root div and instantiate a new board.
+//  */
+// const newGame = function () {
+//   console.log('run');
+//   const root = document.getElementById('root')!;
+//   root.innerHTML = '';
+//   const board = new Board();
+//   board.render(root);
+//   const cpuPlayer = new DumbAIOpponent(PlayerID.playerO, board);
+// };
+
+// // Add click handler to reset button.
+// const resetButton = document.querySelector('#reset');
+// resetButton?.addEventListener('click', newGame);
+
+// newGame();
+
+type gameMode = 'HvH' | 'HvDAI' | 'DAIvH' | 'HvSAI' | 'SAIvH';
+const newGame = function (mode: string) {
   const root = document.getElementById('root')!;
   root.innerHTML = '';
   const board = new Board();
   board.render(root);
-  const cpuPlayer = new DumbAIOpponent(PlayerID.playerO, board);
+  switch (mode) {
+    case 'HvH':
+      break;
+    case 'HvDAI':
+      new DumbAIOpponent(PlayerID.playerO, board);
+      break;
+    case 'DAIvH':
+      new DumbAIOpponent(PlayerID.playerX, board);
+      break;
+    default:
+      console.log('error');
+  }
 };
 
-// Add click handler to reset button.
-const resetButton = document.querySelector('#reset');
-resetButton?.addEventListener('click', newGame);
-
-newGame();
+// Adding submitHandler to game-mode form.
+const gameModeForm = document.getElementById('game-mode');
+gameModeForm?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const checkedButton: HTMLInputElement = document.querySelector(
+    'input[name="game-mode"]:checked'
+  )!;
+  newGame(checkedButton.value);
+});
