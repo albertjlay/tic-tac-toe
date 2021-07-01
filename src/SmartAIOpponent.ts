5057a1a76e1f49deb9f826387cdd1e692706fe90
Albert Lay <albertjlay@gmail.com>
2021-07-01 12:34:34 +0700

    Add SmartAIOpponent for when AI goes second

--- src/SmartAIOpponent.ts
+++ src/SmartAIOpponent.ts
@@ -7,6 +7,14 @@ import Board from './Board';
 import { PlayerID, PositionType, SquareID } from './types';
 
 export default class SmartAIOpponent extends AIOpponent {
+  /**
+   * Chooses a square for the AI's move.
+   * How the algorithm works:
+   *  1. Move to a square which will make it win.
+   *  2. If not above, move to a square which will block player's win
+   *  3. Else, uses the algorithm outlined in https://www.quora.com/Is-there-a-way-to-never-lose-at-Tic-Tac-Toe
+   * @returns square where the AI will make its move
+   */
   getMove() {
     // Take the win.
     const completeAIPattern = this.completePattern(this._myMoves);
@@ -23,8 +31,9 @@ export default class SmartAIOpponent extends AIOpponent {
     if (this.playerID === PlayerID.playerX) {
       return this.getMoveFirst();
     }
+    return this.getMoveSecond();
 
-    return this.board.freeSquares[0];
+    // return this.board.freeSquares[0];
   }
 
   /**
@@ -48,16 +57,16 @@ export default class SmartAIOpponent extends AIOpponent {
   }
 
   /**
-   * Chooses an optimal square fot the AI's move when it moves first.
+   * Chooses an optimal square for the AI's move when it moves first.
    * @returns ID of square to guarantee at least a draw
    */
-  getMoveFirst(): SquareID {
+  private getMoveFirst(): SquareID {
     if (this.playerID === PlayerID.playerO) {
       throw new Error('Only use this method if AI moves first.');
     }
 
     // for first move
-    if (this.board.prevMove === null) {
+    if (this.board.curTurnNumber === 1) {
       return 0;
     }
 
@@ -69,11 +78,11 @@ export default class SmartAIOpponent extends AIOpponent {
   }
 
   /**
-   * Chooses an optimal square fot the AI's move when it moves first and
+   * Chooses an optimal square for the AI's move when it moves first and
    * the player's second move is in the center.
    * @returns ID of square to guarantee at least a draw
    */
-  getMoveFirstCenter(): SquareID {
+  private getMoveFirstCenter(): SquareID {
     if (this.playerID === PlayerID.playerO) {
       throw new Error('Only use this method if AI moves first.');
     } else if (this.board.freeSquares.length === 7 && this.board.prevMove !== 4) {
@@ -92,10 +101,10 @@ export default class SmartAIOpponent extends AIOpponent {
    * the player's second move is not in the center.
    * @returns ID of square to guarantee at least a draw
    */
-  getMoveFirstNonCenter() {
+  private getMoveFirstNonCenter() {
     if (this.playerID === PlayerID.playerO) {
       throw new Error('Only use this method if AI moves first.');
-    } else if (this.board.freeSquares.length === 7 && this.board.prevMove === 4) {
+    } else if (this.board.curTurnNumber >= 2 && Board.getPositionTypeById(this.board.oSquares[0])) {
       throw new Error("Only use this method if the player's second move is non-center");
     }
 
@@ -104,8 +113,100 @@ export default class SmartAIOpponent extends AIOpponent {
       return this.board.freeSquares.includes(id);
     });
 
-    console.log(this.board.freeSquares);
-    console.log(availableCorners);
     return availableCorners[0];
+  }
+
+  /**
+   * Chooses an optimal square fot the AI's move when it moves second.
+   * @returns ID of square to guarantee at least a draw
+   */
+  private getMoveSecond() {
+    if (this.playerID === PlayerID.playerX) {
+      throw new Error('Only use this method if AI moves second.');
+    }
+
+    const firstMoveType = Board.getPositionTypeById(this.board.xSquares[0]);
+    console.log(firstMoveType);
+    switch (firstMoveType) {
+      case PositionType.CENTER:
+        return this.getMoveSecondCenter();
+      case PositionType.CORNER:
+        return this.getMoveSecondCorner();
+      case PositionType.EDGE:
+        return this.getMoveSecondEdge();
+      default:
+        throw new Error(
+          'Position type can only be one of: center, corner, or edge. Please check Board.getPositionTypeById'
+        );
+    }
+  }
+
+  /**
+   * Chooses an optimal square for the AI's move when it moves second and
+   * the player's second move is in the center.
+   * @returns ID of square to guarantee at least a draw
+   */
+  private getMoveSecondCenter() {
+    if (this.playerID === PlayerID.playerX) {
+      throw new Error('Only use this method if AI moves second.');
+    } else if (Board.getPositionTypeById(this.board.xSquares[0]) !== PositionType.CENTER) {
+      throw new Error("Only use this method if the player's first move is center");
+    }
+
+    if (this.board.curTurnNumber === 2) {
+      return 0;
+    } else if (this.board.curTurnNumber === 4) {
+      const corners: SquareID[] = [0, 2, 6, 8];
+      const availableCorners = corners.filter((id) => {
+        return this.board.freeSquares.includes(id);
+      });
+      return availableCorners[0];
+    } else {
+      throw new Error('Error! Please check getMoveSecondCenter');
+    }
+  }
+
+  /**
+   * Chooses an optimal square for the AI's move when it moves second and
+   * the player's second move is in the corner.
+   * @returns ID of square to guarantee at least a draw
+   */
+  private getMoveSecondCorner() {
+    if (this.playerID === PlayerID.playerX) {
+      throw new Error('Only use this method if AI moves second.');
+    } else if (Board.getPositionTypeById(this.board.xSquares[0]) !== PositionType.CORNER) {
+      throw new Error("Only use this method if the player's first move is a corner");
+    }
+
+    const edges: SquareID[] = [1, 3, 5, 7];
+    const availableCorners = edges.filter((id) => {
+      return this.board.freeSquares.includes(id);
+    });
+    if (this.board.curTurnNumber === 2) {
+      return 4;
+    } else if (availableCorners.length !== 0) {
+      return availableCorners[0];
+    } else {
+      return this.board.freeSquares[0];
+    }
+  }
+
+  /**
+   * Chooses an optimal square for the AI's move when it moves second and
+   * the player's second move is in the edge.
+   * @returns ID of square to guarantee at least a draw
+   */
+  private getMoveSecondEdge() {
+    if (this.playerID === PlayerID.playerX) {
+      throw new Error('Only use this method if AI moves second.');
+    } else if (Board.getPositionTypeById(this.board.xSquares[0]) !== PositionType.EDGE) {
+      throw new Error("Only use this method if the player's first move is an edge");
+    }
+
+    if (this.board.curTurnNumber === 2) {
+      return 4;
+    } else {
+      return this.board.freeSquares[0];
+    }
   }
 }
